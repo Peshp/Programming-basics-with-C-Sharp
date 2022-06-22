7e837cdc9fe0b84a1cb11199134e5f2000062cda
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-06-22 11:15:58 +0300

    Add files via upload

---
+++ First Steps in Coding/Exercise/Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceFor1year = double.Parse(Console.ReadLine());
+            var boots = priceFor1year - (priceFor1year * 0.40);
+            var gear = boots - (boots * 0.20);
+            var ball = gear / 4;
+            var accessories = ball / 5;
+            var FinalPrice = priceFor1year + boots + gear + ball + accessories;
+            Console.WriteLine(FinalPrice);
+        }
+    }
+}
