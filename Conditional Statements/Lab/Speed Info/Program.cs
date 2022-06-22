6e2afdc67def38e4c1741d657401cff86c087b9e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-06-22 11:26:11 +0300

    Add files via upload

---
+++ Conditional Statements/Lab/Speed Info/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double V = double.Parse(Console.ReadLine());
+            if(V <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if((V > 10) && (V <= 50))
+            {
+                Console.WriteLine("average");
+            }
+            else if((V > 50) && (V <= 150))
+            {
+                Console.WriteLine("fast");
+            }
+            else if ((V > 150) && (V <= 1000))
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
