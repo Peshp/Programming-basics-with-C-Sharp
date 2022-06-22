de8c7dc09287932b78d4dc30d0c19d932f22d1fa
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-06-22 11:18:08 +0300

    Add files via upload

---
+++ First Steps in Coding/Exercise/Fish Tank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            var V = (a * b * h) / 1000;
+            var Vpercent = percent / 100;
+            var V2 = V - (V * Vpercent);
+            var water = V2 * 1;
+            Console.WriteLine(water);
+        }
+    }
+}
