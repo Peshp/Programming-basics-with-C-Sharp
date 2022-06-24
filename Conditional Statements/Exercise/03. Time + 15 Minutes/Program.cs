47ed9ed3082de25a837d70614c08d1ece0e30e86
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-06-24 23:24:55 +0300

    Add files via upload

---
+++ Conditional Statements/Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TryAgain1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int next = minutes + 15;
+
+            if (next > 60)
+            {
+                int nexthours = hours + 1;
+                int nextminutes = next - 60;
+                
+                if(nextminutes < 10)
+                {
+                    Console.WriteLine($"{nexthours}:0{nextminutes}");
+                }
+              else if(nexthours > 23)
+                {
+                    var hour = 0;
+                    Console.WriteLine($"{hour}:{nextminutes}");
+                }
+                else
+                {
+                    Console.WriteLine($"{nexthours}:{nextminutes}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:{next}");
+            }
+        }
+    }
+}
