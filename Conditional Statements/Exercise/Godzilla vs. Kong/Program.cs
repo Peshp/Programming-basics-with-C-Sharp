941f88252dcaafa52cafa25980c6b587a8bf8b72
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-06-25 22:34:15 +0300

    Add files via upload

---
+++ Conditional Statements/Exercise/Godzilla vs. Kong/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace C16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            double Price = double.Parse(Console.ReadLine());
+
+            double decor = budjet * 0.10;
+            double costums = Price * people;
+            double EvrPrice = decor + costums;          
+            if(people > 150)
+            {
+                double Pricecostums = costums - (costums * 0.10);
+                double FinalPrice = Pricecostums + decor;
+                if(FinalPrice > budjet)
+                {
+                    double left = FinalPrice - budjet;
+                    Console.WriteLine("Not enough money!");
+                    Console.WriteLine($"Wingard needs {left:F2}leva more");
+                }
+                else
+                {
+                    double money = budjet - FinalPrice;
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {money:F2} leva left");
+                }
+            }
+            else
+            {
+                if (EvrPrice > budjet)
+                {
+                    double left = EvrPrice - budjet;
+                    Console.WriteLine("Not enough money!");
+                    Console.WriteLine($"Wingard needs {left:F2}leva more");
+                }
+                else
+                {
+                    double money = budjet - EvrPrice;
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {money:F2} leva left");
+                }
+            }
+        }
+    }
+}
