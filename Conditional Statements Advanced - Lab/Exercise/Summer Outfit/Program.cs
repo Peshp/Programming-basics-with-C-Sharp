22fdb615b7979a211d50c07540f53824592ed45f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-06-30 23:51:53 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/Exercise/Summer Outfit/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Ц32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degree = int.Parse(Console.ReadLine());
+            string DayTime = Console.ReadLine();
+
+            if ((degree >= 10) && (degree <= 18))
+            {
+                switch (DayTime)
+                {
+                    case "Morning":
+                        string Outfit = "Sweatshirt";
+                        string Shoes = "Sneakers";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit} and {Shoes}");
+                        break;
+                    case "Afternoon":
+                        string Outfit2 = "Shirt";
+                        string Shoes2 = "Moccasins";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit2} and {Shoes2}");
+                        break;
+                    case "Evening":
+                        string Outfit3 = "Shirt";
+                        string Shoes3 = "Moccasins";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit3} and {Shoes3}");
+                        break;
+                }
+            }
+            else if ((degree > 18) && (degree <= 24))
+            {
+                switch (DayTime)
+                {
+                    case "Morning":
+                        string Outfit = "Shirt";
+                        string Shoes = "Moccasins";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit} and {Shoes}");
+                        break;
+                    case "Afternoon":
+                        string Outfit2 = "T-Shirt";
+                        string Shoes2 = "Sandals";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit2} and {Shoes2}");
+                        break;
+                    case "Evening":
+                        string Outfit3 = "Shirt";
+                        string Shoes3 = "Moccasins";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit3} and {Shoes3}");
+                        break;
+                }
+            }
+            else
+            {
+                switch (DayTime)
+                {
+                    case "Morning":
+                        string Outfit = "T-Shirt";
+                        string Shoes = "Sandals";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit} and {Shoes}");
+                        break;
+                    case "Afternoon":
+                        string Outfit2 = "Swim Suit";
+                        string Shoes2 = "Barefoot";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit2} and {Shoes2}");
+                        break;
+                    case "Evening":
+                        string Outfit3 = "Shirt";
+                        string Shoes3 = "Moccasins";
+                        Console.WriteLine($"It's {degree} degrees, get your {Outfit3} and {Shoes3}");
+                        break;
+                }
+            }
+        }
+    }
+}
