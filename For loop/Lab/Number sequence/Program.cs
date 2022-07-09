38fb85527f5a057b8470d6f5c8d3773486c56cdc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-09 19:06:03 +0300

    Add files via upload

---
+++ For loop/Lab/Number sequence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            int biggest = int.MaxValue;
+            int smallest = int.MinValue;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if(smallest < num)               
+                    smallest = num;                
+                if(biggest > num)                
+                    biggest = num;             
+            }
+            Console.WriteLine($"Max number: {smallest}");
+            Console.WriteLine($"Min number: {biggest}");
+        }
+    }
+}
