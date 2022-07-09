547850a2d0af74961dff4edfd26f499fbc73d5bc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-09 19:07:09 +0300

    Add files via upload

---
+++ For loop/Lab/Left and Right Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace C47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());         
+            var sum = 0;
+            var sum2 = 0;                   
+            for (int i = 0; i < n; i++)
+            {                                             
+                 sum += int.Parse(Console.ReadLine());                                            
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum2 += int.Parse(Console.ReadLine());
+            }
+            if (sum == sum2)
+                Console.WriteLine("Yes, sum = " + sum);
+            else
+            {
+                int diff = Math.Abs(sum2 - sum);
+                Console.WriteLine("No, diff = " + diff);
+            }
+        }
+    }
+}
