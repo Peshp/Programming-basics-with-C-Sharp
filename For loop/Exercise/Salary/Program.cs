dd91a9df87f29a199774bc3e34d8694425454b93
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-10 14:25:17 +0300

    Add files via upload

---
+++ For loop/Exercise/Salary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace C52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            double moneyleft = 0;
+            int fine = 0;
+
+            for (int i = 0; i < tabs; i++)
+            {
+                string site = Console.ReadLine();
+                switch (site)
+                {
+                    case "Facebook":
+                        fine = 150;
+                        salary -= fine;
+                        moneyleft = salary;
+                        if (salary == 0)
+                        {
+                            Console.WriteLine("You have lost your salary.");
+                            return;
+                        }
+                        else
+                        {
+                            moneyleft -= fine;
+                        }
+                        break;
+                    case "Instagram":
+                        fine = 100;
+                        salary -= fine;
+                        moneyleft = salary;
+                        if (salary == 0)
+                        {
+                            Console.WriteLine("You have lost your salary.");
+                            return;
+                        }
+                        else
+                        {
+                            moneyleft -= fine;
+                        }
+                        break;
+                    case "Reddit":
+                        fine = 50;
+                        salary -= fine;
+                        moneyleft = salary;
+                        if (salary == 0)
+                        {
+                            Console.WriteLine("You have lost your salary.");
+                            return;
+                        }
+                        else
+                        {
+                            moneyleft -= fine;
+                        }
+                        break;                  
+                }
+            }
+            Console.WriteLine($"{moneyleft:f0}");
+        }
+    }
+}
