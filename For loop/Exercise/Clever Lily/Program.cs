92fff9dda4270a753cf78e88fd5432c266062ede
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-10 14:23:52 +0300

    Add files via upload

---
+++ For loop/Exercise/Clever Lily/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace C51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double DressWash = double.Parse(Console.ReadLine());
+            int Toy = int.Parse(Console.ReadLine());
+            int toy = 0;
+            double cash = 0;
+            double moneyGift = 10;
+
+            for (int i = 1; i <= age; i++)
+            {
+                int reminder = i % 2;
+                if(reminder == 1)
+                {
+                    toy++;
+                }
+                else
+                {
+                    cash += moneyGift;
+                    cash -= 1;
+                    moneyGift += 10;
+                }
+            }
+            double ToysPrice = toy * Toy;
+            double money = ToysPrice + cash;
+
+            if(money >= DressWash)
+            {
+                double left = money - DressWash;
+                Console.WriteLine($"Yes! {left:f2}");
+            }
+            else
+            {
+                double notenough = DressWash - money;
+                Console.WriteLine($"No! {notenough:f2}");
+            }
+        }
+    }
+}
