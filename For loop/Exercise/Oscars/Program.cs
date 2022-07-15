e2de2c9bd93118b603194439db79ad365b2ea665
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-15 22:02:31 +0300

    Add files via upload

---
+++ For loop/Exercise/Oscars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actor = Console.ReadLine();
+            double points = double.Parse(Console.ReadLine());
+            int rate = int.Parse(Console.ReadLine());
+
+            double Finalpoints = points;
+
+            bool PointsForWin = false;
+
+            for (int i = 0; i < rate; i++)
+            {
+                string rateName = Console.ReadLine();
+                double ratePoints = double.Parse(Console.ReadLine());
+
+                double Jurypoints = rateName.Length * ratePoints / 2;
+                Finalpoints += Jurypoints;
+
+                if (Finalpoints > 1250.5)
+                {
+                    PointsForWin = true;                
+                    break;
+                }
+            }           
+            if(PointsForWin)
+            {
+                Console.WriteLine($"Congratulations, {actor} got a nominee for leading role with {Finalpoints:f1}!");
+            }
+            else
+            {
+                double LeftPoints = 1250.5 - Finalpoints;
+                Console.WriteLine($"Sorry, {actor} you need {LeftPoints} more!");
+            }
+        }
+    }
+}
