6360c43c3b1bf930db81ace0e5ab50ffd6dca5fc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-17 20:51:50 +0300

    Add files via upload

---
+++ While loop/Lab/Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while(sum < num)
+            {
+                int othernums = int.Parse(Console.ReadLine());
+                sum += othernums;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
