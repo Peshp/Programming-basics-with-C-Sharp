fb9177a145e6743a12555072b967321ce00c18e0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-18 12:01:58 +0300

    Add files via upload

---
+++ While loop/Lab/Graduation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double grades = 0.00;
+            int clase = 0;
+            double average = 0;           
+            while (true)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                grades += grade;
+                clase += 1;
+                if(grade < 4)
+                {
+                    Console.WriteLine($"{name} has been excluded at {clase} grade");
+                    break;
+                }
+                if(clase == 12)
+                {
+                    average = grades / clase;
+                    Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+                    break;
+                }
+            }
+        }
+    }
+}
