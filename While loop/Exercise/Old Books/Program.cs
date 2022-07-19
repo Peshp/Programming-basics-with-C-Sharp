793a4c02ef4e36adb788a66b4b195abfc9fe437a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-19 12:10:32 +0300

    Add files via upload

---
+++ While loop/Exercise/Old Books/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace C64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int counter = 0;
+            bool bookfound = false;
+            while (true)
+            {
+                string books = Console.ReadLine();
+                counter++;
+                if(books == book)
+                {
+                    counter -= 1;
+                    bookfound = true;
+                    break;
+                }
+                if(books == "No More Books")
+                {
+                    counter -= 1;
+                    break;
+                }
+            }
+            if(bookfound)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}
