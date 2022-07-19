72df70f1e4b8bc23e294d5c3da1eccf40c8cf8d0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-19 12:11:44 +0300

    Add files via upload

---
+++ While loop/Exercise/Exam Preparation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace C65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numgrade = int.Parse(Console.ReadLine());
+            bool enough = false;
+            double grades = 0;
+            double gradesum = 0;
+            int exams = 0;
+            string lastproblem = "";
+            int badgrades = 0;
+            double average = 0;
+            while (true)
+            {
+                string examname = Console.ReadLine();
+                grades++;              
+                if(examname == "Enough")
+                {
+                    enough = true;
+                    break;
+                }              
+                int grade = int.Parse(Console.ReadLine());
+                if (grade <= 4)
+                {
+                    badgrades++;
+                    if(badgrades == numgrade)
+                    {
+                        break;
+                    }
+                }
+                gradesum += grade;                
+                exams++;
+                lastproblem = examname;
+                average = gradesum / grades;
+            }           
+            if(enough)
+            {
+                Console.WriteLine($"Average score: {average:f2}");
+                Console.WriteLine($"Number of problems: {exams}");
+                Console.WriteLine($"Last problem: {lastproblem}");
+            }
+            else if(badgrades == numgrade)
+            {
+                Console.WriteLine($"You need a break, {badgrades} poor grades."); 
+            }
+        }
+    }
+}
