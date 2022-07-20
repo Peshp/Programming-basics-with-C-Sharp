0bb7145e37b166cfb8e4a3c46feb018e267d3c3b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-20 13:58:43 +0300

    Add files via upload

---
+++ While loop/Exercise/Moving/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int space = a * b * h;
+            int package = 0;
+            int morespace = 0;
+            string command = "";
+            while (package < space)
+            {
+                command = Console.ReadLine();
+                if(command == "Done")
+                {
+                    int leftspace = space - package;
+                    Console.WriteLine($"{leftspace} Cubic meters left.");
+                    break;
+                }
+                int packagespace = int.Parse(command);
+                package += packagespace;
+            }
+            if(package >= space)
+            {
+                morespace = Math.Abs(space - package);
+                Console.WriteLine($"No more free space! You need {morespace} Cubic meters more.");
+            }           
+        }
+    }
+}
