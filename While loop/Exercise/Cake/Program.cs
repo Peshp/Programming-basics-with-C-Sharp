b3638262cbb4b7e49a6a459b529df561cbd45be5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-20 13:57:45 +0300

    Add files via upload

---
+++ While loop/Exercise/Cake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double cake = a * b;           
+            double left = 0;
+            double pieces = 0;
+            string stop = "";
+            while (pieces < cake)
+            {
+                stop = Console.ReadLine();
+                if(stop == "STOP")
+                {
+                    left = cake - pieces;
+                    Console.WriteLine($"{left} pieces are left.");
+                    break;
+                }
+                int get = int.Parse(stop);
+                pieces += get;                                                               
+            }
+            if(pieces > cake)
+            {
+                left = Math.Abs(cake - pieces);
+                Console.WriteLine($"No more cake left! You need {left} pieces more.");
+            }          
+        }
+    }
+}
