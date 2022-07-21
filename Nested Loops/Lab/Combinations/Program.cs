ce168b155ccddc54300026e8acb4ff71cb42b1ea
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-21 13:08:30 +0300

    Add files via upload

---
+++ Nested Loops/Lab/Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int b = 0;
+            int result = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 25; i++)
+            {              
+                for (int j = 0; j <= n; j ++)
+                {                                      
+                    for (int h = 0; h <= n; h++)
+                    {
+                        result = i + j + h;                       
+                        if(result == n)
+                        {
+                            b += 1;                            
+                        }
+                    }                                  
+                }                           
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
