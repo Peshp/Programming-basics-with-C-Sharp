f77f5c764832b525a9ee9654e40a7e04801407f7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-22 12:31:53 +0300

    Add files via upload

---
+++ Nested Loops/Lab/Travelling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if(destination == "End")
+                {
+                    break;
+                }
+
+                double budjet = double.Parse(Console.ReadLine());
+                double money = 0;
+
+                while (true)
+                {
+                    if(money >= budjet)
+                    {
+                        break;
+                    }
+                    money += double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+            }          
+        }
+    }
+}
