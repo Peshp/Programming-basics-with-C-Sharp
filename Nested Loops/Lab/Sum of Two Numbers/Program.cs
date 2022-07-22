ba46134c6d581a4ad88217937424f47ce1899960
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-22 12:30:57 +0300

    Add files via upload

---
+++ Nested Loops/Lab/Sum of Two Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int magical = int.Parse(Console.ReadLine());
+            int result = 0;
+            int magicnum = 0;
+            int d = 0;
+            int c = 0;
+            bool found = false;
+
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                    result = j + i;
+                    magicnum++;
+                    if (result == magical)
+                    {                      
+                        d = i;
+                        c = j;
+                        Console.WriteLine($"Combination N:{magicnum} ({d} + {c} = {result})");
+                        found = true;
+                        break;
+                    }                                    
+                }
+                if (found)
+                    break;
+            } 
+            if(found == false)
+            {
+                Console.WriteLine($"{magicnum} combinations - neither equals {magical}");
+            }
+        }
+    }
+}
