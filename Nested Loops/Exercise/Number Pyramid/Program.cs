27fe123ba01c206c59c1d1bf605572503a33872f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-24 21:25:58 +0300

    Add files via upload

---
+++ Nested Loops/Exercise/Number Pyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool bigger = false;
+            int current = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if(current > n)
+                    {
+                        bigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                if (bigger)
+                    break;
+                Console.WriteLine();
+            }
+        }
+    }
+}
