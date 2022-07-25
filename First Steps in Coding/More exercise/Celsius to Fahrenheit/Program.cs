85ed622fab0794abf97e4bad79e5a3ac89785bc0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-25 13:24:24 +0300

    Add files via upload

---
+++ First Steps in Coding/More exercise/Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoreC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C = double.Parse(Console.ReadLine());
+            var F = (C * 1.8) + 32;
+            Console.WriteLine($"{F:f2}");
+        }
+    }
+}
