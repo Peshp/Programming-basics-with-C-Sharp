fca5088c115c63c5454b00d807ca5ecbe53b19a2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-25 13:22:36 +0300

    Add files via upload

---
+++ First Steps in Coding/More exercise/Trapeziod Area/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoreC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = (a + b) * h / 2;
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
