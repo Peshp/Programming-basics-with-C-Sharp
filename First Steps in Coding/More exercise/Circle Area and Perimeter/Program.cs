f10f20297f179f2e9fba419567a863f72ae72231
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-26 16:06:57 +0300

    Add files via upload

---
+++ First Steps in Coding/More exercise/Circle Area and Perimeter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoreC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double P = Math.PI * r * 2;
+            double S = Math.PI * r * r;
+            Console.WriteLine($"{S:f2}");
+            Console.WriteLine($"{P:f2}");
+        }
+    }
+}
