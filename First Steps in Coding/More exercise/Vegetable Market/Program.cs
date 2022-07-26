8212cf127f5e2395f386d5eb1354c7f5f6502772
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-26 16:03:25 +0300

    Add files via upload

---
+++ First Steps in Coding/More exercise/Vegetable Market/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MoreC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double VegPrice = double.Parse(Console.ReadLine());
+            double FruPrice = double.Parse(Console.ReadLine());
+            double VegFullPrice = double.Parse(Console.ReadLine());
+            double FruFullPrice = double.Parse(Console.ReadLine());
+
+            var FinalPriceVeg = VegPrice * VegFullPrice;
+            var FinalPriceFru = FruPrice * FruFullPrice;
+            Console.WriteLine($"{((FinalPriceVeg + FinalPriceFru) / 1.94):f2}");
+        }
+    }
+}
