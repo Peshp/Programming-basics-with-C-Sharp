2800e3846b1d8f3da903c69c72d5ce8bcd670c49
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-29 08:24:58 +0300

    Add files via upload

---
+++ Conditional Statements/More exercise/Transport Price/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MoreC13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            double taxi = 0;
+            double bus = 0.09;
+            double train = 0.06;
+         
+            if(n < 20)
+            {
+                switch (time)
+                {
+                    case "day":
+                        taxi = (0.79 * n) + 0.70;
+                        break;
+                    case "night":
+                        taxi = (0.90 * n) + 0.70;
+                        break;                  
+                }
+                Console.WriteLine($"{taxi:f2}");
+            }
+            else if((n >= 20) && (n < 100))
+            {
+                bus *= n;
+                Console.WriteLine($"{bus:f2}");
+            }
+            else
+            {
+                train *= n;
+                Console.WriteLine($"{train:f2}");
+            }          
+        }
+    }
+}
