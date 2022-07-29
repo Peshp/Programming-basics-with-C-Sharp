f4d362ac3ce039ff763f5bedee48a2b0397c97f2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-29 08:25:46 +0300

    Add files via upload

---
+++ Conditional Statements/More exercise/Pets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MoreC15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int Foodkg = int.Parse(Console.ReadLine());
+            double FoodDog = double.Parse(Console.ReadLine());
+            double FoodCat = double.Parse(Console.ReadLine());
+            double FoodTurt = double.Parse(Console.ReadLine());
+
+            double turt = FoodTurt / 1000;
+
+            double Dog = FoodDog * days;
+            double Cat = FoodCat * days;
+            double Turtle = turt * days;
+            double AllFood = Dog + Cat + Turtle;         
+            double left = 0;
+
+            if(AllFood < Foodkg)
+            {
+                left = Math.Floor(Foodkg - AllFood);
+                Console.WriteLine($"{Math.Abs(left):f0} kilos of food left.");
+            }
+            else
+            {
+                left = Math.Ceiling(AllFood - Foodkg);
+                Console.WriteLine($"{Math.Abs(left):f0} more kilos of food are needed.");
+            }
+        }
+    }
+}
