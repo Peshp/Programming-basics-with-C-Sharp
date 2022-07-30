479453c75a7cc8069ea3a73e3d19cbbdb2016de2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-30 08:55:59 +0300

    Add files via upload

---
+++ Conditional Statements/More exercise/Flower Shop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MoreC16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Magns = int.Parse(Console.ReadLine());
+            int zumbs = int.Parse(Console.ReadLine());
+            int roses = int.Parse(Console.ReadLine());
+            int cactus = int.Parse(Console.ReadLine());
+            double GiftPrice = double.Parse(Console.ReadLine());
+
+            double Price = Magns * 3.25 + zumbs * 4 + roses * 3.5 + cactus * 8;
+            double danuk = Price * 0.05;
+            double Money = Price - danuk;
+            double left;
+
+            if(Money >= GiftPrice)
+            {
+                left = Math.Floor(Money - GiftPrice);
+                Console.WriteLine($"She is left with {left} leva.");
+            }
+            else
+            {
+                left = Math.Ceiling(GiftPrice - Money);
+                Console.WriteLine($"She will have to borrow {left} leva.");
+            }
+        }
+    }
+}
