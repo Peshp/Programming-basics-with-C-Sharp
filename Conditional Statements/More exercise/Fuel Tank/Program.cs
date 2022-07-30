41694675f5e532907e383f5c36ebf18becccd3c3
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-30 08:56:53 +0300

    Add files via upload

---
+++ Conditional Statements/More exercise/Fuel Tank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MoreC17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuel = Console.ReadLine();
+            double litres = int.Parse(Console.ReadLine());
+
+            if(litres >= 25)
+            {
+                switch (fuel)
+                {
+                    case "Diesel":
+                        Console.WriteLine("You have enough diesel.");
+                        break;
+                    case "Gasoline":
+                        Console.WriteLine("You have enough gasoline.");
+                        break;
+                    case "Gas":
+                        Console.WriteLine("You have enough gas.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid fuel!");
+                        break;
+                }
+                
+            }
+            else
+            {
+                switch(fuel)
+                {
+                    case "Diesel":
+                        Console.WriteLine("Fill your tank with diesel!");
+                        break;
+                    case "Gasoline":
+                        Console.WriteLine("Fill your tank with gasoline!");
+                        break;
+                    case "Gas":
+                        Console.WriteLine("Fill your tank with gas!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid fuel!");
+                        break;
+                }              
+            }
+        }
+    }
+}
