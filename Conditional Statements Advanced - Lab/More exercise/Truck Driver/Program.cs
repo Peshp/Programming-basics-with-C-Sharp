e6c065f1036929499901ce96a234ab85b75006ea
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-01 07:15:27 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/More exercise/Truck Driver/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MoreC24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double km = double.Parse(Console.ReadLine());
+
+            double price = 0.00;          
+
+            if(km < 5001)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                    case "Autumn":
+                        price = 0.75 * km * 4;
+                        break;
+                    case "Summer":
+                        price = 0.9 * km * 4;
+                        break;
+                    case "Winter":
+                        price = 1.05 * km * 4;
+                        break;
+                }
+            }
+            else if (km < 10001 && km > 5000)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                    case "Autumn":
+                        price = 0.95 * km * 4;
+                        break;
+                    case "Summer":
+                        price = 1.1 * km * 4;
+                        break;
+                    case "Winter":
+                        price = 1.25 * km * 4;
+                        break;
+                }
+            }
+            else if (km < 20001 && km > 10000)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                    case "Autumn":                      
+                    case "Summer":                    
+                    case "Winter":
+                        price = 1.45 * km * 4;
+                        break;
+                }
+            }
+            price -= price * 0.1;
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
