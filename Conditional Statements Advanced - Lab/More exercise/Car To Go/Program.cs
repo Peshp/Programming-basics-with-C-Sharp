5ec92cf8a2ba75a8fc26fcc328c25baf25237f3a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-01 07:14:30 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/More exercise/Car To Go/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MoreC22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double Price = 0;         
+            string car = "kola";
+
+            if(budjet < 101)
+            {
+                switch(season)
+                {
+                    case "Summer":
+                        Price = budjet * 0.35;
+                        car = "Cabrio";
+                        break;
+                    case "Winter":
+                        Price = budjet * 0.65;
+                        car = "Jeep";
+                        break;
+                }
+                Console.WriteLine("Economy class");
+                Console.WriteLine($"{car} - {Price:f2}");
+            }
+            else if (budjet > 100 && budjet < 501)
+            {
+                switch (season)
+                {
+                    case "Summer":
+                        Price = budjet * 0.45;
+                        car = "Cabrio";
+                        break;
+                    case "Winter":
+                        Price = budjet * 0.8;
+                        car = "Jeep";
+                        break;
+                }
+                Console.WriteLine("Compact class");
+                Console.WriteLine($"{car} - {Price:f2}");
+            }
+            else
+            {
+                switch (season)
+                {
+                    case "Summer":                      
+                    case "Winter":
+                        Price = budjet * 0.9;
+                        car = "Jeep";
+                        break;
+                }
+                Console.WriteLine("Luxury class");
+                Console.WriteLine($"{car} - {Price:f2}");
+            }
+        }
+    }
+}
