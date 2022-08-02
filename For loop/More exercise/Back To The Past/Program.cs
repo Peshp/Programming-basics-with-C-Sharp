93e178cacc45183be91df437196b0902b14e7afd
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-02 08:44:49 +0300

    Add files via upload

---
+++ For loop/More exercise/Back To The Past/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace remove_extra_space_in_string                            
+
+{
+    class remove_space                                           
+    {
+        public void replace(string str1)                         
+        {
+            string pattern = "\\s+";
+            string replacement = " ";                       
+            Regex rx = new Regex(pattern);
+            string result = rx.Replace(str1, replacement);  
+
+            Console.WriteLine("Your string String          : {0}", str1);
+            Console.WriteLine("Remove extra space String   : {0}", result);
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any string with extra space");
+
+            string readstr = Console.ReadLine();
+            remove_space rs = new remove_space();
+            rs.replace(readstr);
+        }
+    }
+}
