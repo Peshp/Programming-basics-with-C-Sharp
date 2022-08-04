ef141763c38ac9fe88a099a84d98d54509ab953a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-04 12:04:14 +0300

    Add files via upload

---
+++ For loop/More exercise/Bills/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MoreC32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int months = int.Parse(Console.ReadLine());
+
+            double elPrice = 0.00;
+            double waterPrice = 0.00;
+            double netPrice = 0.00;
+            double otherPrice = 0.00;
+            double average = 0.00;
+
+            for (int i = 0; i < months; i++)
+            {
+                double el = double.Parse(Console.ReadLine());
+
+                elPrice += el;
+                waterPrice = months * 20;
+                netPrice = months * 15;
+                otherPrice = (elPrice + waterPrice + netPrice) * 0.2 + (elPrice + waterPrice + netPrice);
+                average = (elPrice + waterPrice + netPrice + otherPrice) / months;
+            }
+            Console.WriteLine($"Electricity: {elPrice:f2} lv");
+            Console.WriteLine($"Water: {waterPrice:f2} lv");
+            Console.WriteLine($"Internet: {netPrice:f2} lv");
+            Console.WriteLine($"Other: {otherPrice:f2} lv");
+            Console.WriteLine($"Average: {average:f2} lv");
+        }
+    }
+}
